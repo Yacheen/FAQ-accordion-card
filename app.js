95093bd10544f2a693d4ef170d1efe1f80fa1c18
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-05-14 22:09:57 -0600

    what do i even put here

---
+++ app.js
@@ -0,0 +1,17 @@
+// create buttons/listeners that detect when arrow is clicked with a transition,
+// then display: block, when clicked again, display: none;
+
+//event listener text to use in order: 
+
+//You can invite up to 2 additional users on the Free plan. There is no limit on 
+//team members for the Premium plan.
+
+// No more than 2GB. All files in your account must fit your allotted storage space.
+
+//Click “Forgot password” from the login page or “Change password” from your profile page.
+//A reset link will be emailed to you.
+
+// Yes! Send us a message and we’ll process your request no questions asked.
+
+// Chat and email support is available 24/7. Phone lines are open during normal business hours.
+
