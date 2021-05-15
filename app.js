53ee89a93a389ce9364bea0968c96e8592644a8d
Xenon <gonda.armand@gmail.com>
2021-05-15 19:45:17 +0200

    Rewrote loop. Removed unnecessary JS.

--- app.js
+++ app.js
@@ -17,25 +17,30 @@
 // create the li classlists, select the ul first maybe
 const faqQuestions = document.getElementById('FAQ-questions');
 
-const liItems = faqQuestions.children;
-console.log(liItems);
-
-Array.from(liItems).forEach((element, index) => {
-    
-    
-    //create a div with a class li-item-styles
-    const itemDiv = document.createElement('div');
-    itemDiv.classList.add('li-item-styles');
-
+const liItems = document.querySelectorAll(".question");
+for (const [x, li] of Object.entries(liItems)) {
+    console.log(x, li)
     //add classes to the li's
-    element.classList.add('item');
-    element.classList.add(`item${index+1}`);
+    li.classList.add('item');
+    li.classList.add(`item${x}`);
+}
+// liItems.forEach((element, index) => {
+//     console.log(element, index)
+    
+    
+//     //create a div with a class li-item-styles
+//     const itemDiv = document.createElement('div');
+//     itemDiv.classList.add('li-item-styles');
 
-    //create the dropdown menu buttons, and append them to the divs
-    const itemDropdownArrow = document.createElement('img');
-    itemDropdownArrow.classList.add('img-arrow-background');
-    itemDiv.appendChild(itemDropdownArrow);
+//     //add classes to the li's
+//     element.classList.add('item');
+//     element.classList.add(`item${index+1}`);
 
-    console.log(itemDiv.children);
+//     //create the dropdown menu buttons, and append them to the divs
+//     const itemDropdownArrow = document.createElement('img');
+//     itemDropdownArrow.classList.add('img-arrow-background');
+//     itemDiv.appendChild(itemDropdownArrow);
 
-});
+//     console.log(itemDiv.children);
+
+// });
